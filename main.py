c6ddab0d088f12cae2786b03184669f510ad00c8
jogog <jogog007@mail.ru>
2020-02-11 00:27:43 +0300

    smth new

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ TOKEN = '1029910258:AAEZMySWWxqScBiYXr4UkyqRwiIYppgMkA4'
 
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты можешь использовать /help для просмотра функций')
+
+@bot.message_handler(commands=['help'])
+def test(message):
+	bot.send_message(message.chat.id, 'sosi pisos')
+
 @bot.message_handler(commands=['test'])
 def test(message):
 	dls = 'https://drive.google.com/file/d/1X4X1a1jUDekW2UDyoZaVTR8WyuAWkrUh'
