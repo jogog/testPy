7a0b36327052e09e1f5019b8fbf5d72d0c651d58
jogog <jogog007@mail.ru>
2020-02-11 00:12:35 +0300

    add telegrambot

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import urllib
 import requests
-dls = 'https://drive.google.com/file/d/1X4X1a1jUDekW2UDyoZaVTR8WyuAWkrUh'
-urllib.request.urlretrieve(dls, "test.xls")
+import telebot
+import urllib
+import requests
+
+TOKEN = '1029910258:AAEZMySWWxqScBiYXr4UkyqRwiIYppgMkA4'
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['test'])
+def test(message):
+	dls = 'https://drive.google.com/file/d/1X4X1a1jUDekW2UDyoZaVTR8WyuAWkrUh'
+	urllib.request.urlretrieve(dls, "test.xls")
+	bot.send_message(message.chat.id, 'GAGAGAGA')
+
+
+bot.polling()
